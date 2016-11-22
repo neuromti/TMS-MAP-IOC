b9b38b44e4c239b3e71ec30f418030a798d12dac
agricolab <robertsadresse@gmx.de>
2016-11-22 12:59:11 +0100

    Interpolation for MEP, Latency and Amplitude finished
    Added two +utils/functions
    - distanceweights_groupgrid for weight-calculation
    - interpolate_latency for vicinity-based projection
    
    Tested functionality and performance and accordingly set the script

---
+++ code/+utils/distanceweights_groupgrid.m
@@ -0,0 +1,43 @@
+function [quadratic_weights,thresholded_vicinity,linear_weights] = distanceweights_groupgrid(sub_xyz,group_xyz,thresh_mm)
+% Calculates for all points on the group grid the normalized weights based on distance
+% @param sub_xyz is the individuals mapping grid
+% @param group_xyz the mapping grid for the whole group
+% @param thresh_mm is the distance threshold: default threshold is 50 mm
+% @returns quadratic_weigths as matrix of normalized weights (squared distance) 
+% @returns utresholded_weigths as matrix of normalized weights (linear distance) 
+% @returns thresholded_vicinity as matrix of grid points in vicinity of
+
+if nargin <3, thresh_mm = 50; end  
+
+%calculate distance for all points in the group-grid
+all_dist                = pdist2(sub_xyz,group_xyz)';
+
+if nargout>=1,
+    t_quad_invdist          = (1./(all_dist.^2));
+end
+
+if nargout>=3,
+    t_linear_invdist        = (1./all_dist);
+end
+
+
+% Calculate quadratic Weights based on inverse of quadratic distance
+if nargout>=1,
+    t_norm                  = repmat(sum(t_quad_invdist,2),1,size(t_quad_invdist,2));
+    quadratic_weights       = (t_quad_invdist./t_norm);
+    clear t_norm t_quad_invdist
+end
+
+% Calculate thresholded Weights based on distance for Latency 
+% thresholded for maximal accepted distance
+if nargout>=2,
+    thresholded_vicinity    = all_dist<=thresh_mm;
+end
+
+% Calculate unthresholded linear Weights based on inverse of distance
+% Consider that weights should be a weighted average, i.e. norm(x,1) should be equal to 1;
+if nargout>=3,
+    t_norm                  = repmat(sum(t_linear_invdist,2),1,size(t_linear_invdist,2));
+    linear_weights          = (t_linear_invdist./t_norm);
+    clear t_norm t_linear_invdist
+end
--- code/process_study_mapping.m
+++ code/process_study_mapping.m
@@ -3,11 +3,17 @@
 addpath('C:\Users\Robert Bauer\Documents\Matlab\private_toolbox');
 cls; %clc, clear all, close all, matlabrc, fclose('all'), addpath of Fieldtrip and other toolboxes, ft_defaults
 addpath('C:\PROJECTS\Subject Studies\TMS-MAP-IOC\code'); %to access the package folder +utils
+
+addpath('C:\Users\Robert Bauer\Documents\MATLAB\other_toolboxes\CETperceptual_MATLAB'); %folder with colormaps
+cmap        = linear_kry_5_95_c72_n256;
+set(groot,'DefaultFigureColormap',cmap)
+
 load('C:\PROJECTS\Subject Studies\TMS-MAP-IOC\code\config.mat','headmodel','setup','folder');
 
 %% loading data
 logfilename = [folder.code,'Logfile.log'];
 logfileid   = fopen(logfilename,'wt');
+fprintf(logfileid,'started script on %s \n',datetime('now'));
 
 % read in the datafiles int the directory
 D           = dir(folder.data.map);
@@ -19,62 +25,121 @@ IsArtifacted = utils.check_artifacts(folder.data.map);
 % Define the outlines of the group-wise grid for projection of individual
 % maps
 [grid_xyz,map_dimensions,X,Y,Z] = utils.make_groupgrid(folder.data.map,IsArtifacted);
-
-%define some parameters and anonymous functions
-do_norm         = @(x)x/norm(x,2);    
-accepted_dist   = 15; %1.5 cm distance
-
-
+reshape_to_grid = @(x)reshape(x,map_dimensions(1),map_dimensions(2),map_dimensions(3));
 
 clear IPOL 
-
 for idx_dataset = 1:length(D),    
        
-    % arrange and set index variables
-    s               = regexp(D(idx_dataset).name,'S\w*');
-    c               = regexp(D(idx_dataset).name,'C\w*');
-    d               = regexp(D(idx_dataset).name,'.mat');
-    idx_data_sub    = int32(str2double(D(idx_dataset).name(s+1:c-1)));
-    idx_data_cond   = int32(str2double(D(idx_dataset).name(c+1:d-1)));
-    idx_sub         = find(setup.SUB.id==idx_data_sub);
-       
     % load dataset if it is not artifacted
-    if artifacted{idx_dataset},
+    if IsArtifacted(idx_dataset),
         continue;
     end
     load([folder.data.map,D(idx_dataset).name]);
-
+     
+    % Arrange and set index variables
+    % Glue conditons and location to data    
+    % @return IPOL.HS and IPOL.ANT as HotSpot and Anterior Location in xyz  
+    % @return IPOL.COND as Condition string
+    % @return IPOL.DESIGN as Design Matrix (Biphasic / Lateromedial) yes/no
+    t.s                     = regexp(D(idx_dataset).name,'S\w*');
+    t.c                     = regexp(D(idx_dataset).name,'C\w*');
+    t.d                     = regexp(D(idx_dataset).name,'.mat');
+    i.data_sub              = int32(str2double(D(idx_dataset).name(t.s+1:t.c-1)));
+    i.data_cond             = int32(str2double(D(idx_dataset).name(t.c+1:t.d-1)));
+    i.sub                   = find(setup.SUB.id==i.data_sub);
+    t.sex                   = setup.SUB.sex(i.sub);
+    t.age                   = setup.SUB.age(i.sub);
+    t.idx                   = i.data_sub;
+    t.pos                   = setup.SUB.pos{i.sub};
+    t.HS                    = [t.pos{i.data_cond+1,2:4}];
+    t.ANT                   = [t.pos{i.data_cond+1,5:7}];
     
-    collect_XYZ     = NaN(size(mapping.xyz,1),size(grid_xyz,1));
-    all_dist        = pdist2(mapping.xyz,grid_xyz);
-    t_accepted      = ~(all_dist>accepted_dist);
-    t_not_incl      = (all(~t_accepted,1));
-    i_not_incl      = find(~t_not_incl);
-    for idx_coordinate=i_not_incl,
-             
-        t_dist      = all_dist(:,idx_coordinate);        
-        t_invdist   = 1./t_dist;
-        t_remv      = ~t_accepted(:,idx_coordinate);
-        t_invdist(t_remv)=[];
-        t_weight    = do_norm((t_invdist));
-        t_interpol  = t_weight'*mapping.amp(~t_remv);
-        collect_XYZ(find(~t_remv),idx_coordinate) = t_interpol;
-   
+    IPOL(idx_dataset).HS        = t.HS;
+    IPOL(idx_dataset).ANT       = t.ANT;
+    IPOL(idx_dataset).COND      = setup.MAP.label.all{i.data_cond};
+    IPOL(idx_dataset).DESIGN    = logical([setup.MAP.BI(i.data_cond),setup.MAP.LM(i.data_cond)]);
+    IPOL(idx_dataset).sub_sex   = t.sex;
+    IPOL(idx_dataset).sub_age   = t.age;
+    IPOL(idx_dataset).sub_idx   = t.idx;
+    
+    clear t i
+
+    % Interpolate individual subjects amplitude and MEP+ on the group grid based on squared distance
+    % Interpolate individual subjects latency after centering on average
+    quad_weights                =  utils.distanceweights_groupgrid(mapping.xyz,grid_xyz,25);
+
+    sub_mep                     = double(mapping.amp>50 & mapping.lat>15);    
+    sub_lat                     = mapping.lat;
+    sub_lat(sub_lat>0)          = sub_lat(sub_lat>0)-mean(sub_lat(sub_lat>0));
+    
+    IPOL(idx_dataset).quad_AMP  = reshape_to_grid((quad_weights*mapping.amp));       
+    IPOL(idx_dataset).quad_MEP  = reshape_to_grid((quad_weights*sub_mep));     
+    IPOL(idx_dataset).quad_LAT  = reshape_to_grid((quad_weights*sub_lat));     
+    
+    fprintf(logfileid,'At %s finished dataset %i \n',datetime('now'),idx_dataset);
+end
+fclose(logfileid);
+save([folder.results.stats,'map_interpolated.mat'],'IPOL')
+%%
+DESIGN  = cat(1,IPOL.DESIGN);
+SUB     = cat(1,IPOL.sub_idx);
+AMP     = cat(4,IPOL.quad_AMP);
+LAT     = cat(4,IPOL.quad_LAT);
+MEP     = cat(4,IPOL.quad_MEP);
+
+%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%%
+F = [];
+for i_x=1:map_dimensions(1),
+    for i_y=1:map_dimensions(2),
+        %for i_z=1:map_dimensions(3),
+        i_z = 1;
+            [p,tab,stats]       = anovan(squeeze(nanmean(MEP(i_x,i_y,:,:),3)),cat(2,DESIGN,SUB),'display','off','model',[1 0 0;0 1 0;1 1 0;0 0 1]);
+            F(i_x,i_y,i_z,:)    = [tab{2:4,6}];
+        %end
     end
-    
+end
 
-    
-    f_interpolated =  (nanmean(collect_XYZ,1));
-    IPOL(idx_dataset).DATA  = f_interpolated;
-    IPOL(idx_dataset).SUB   = idx_sub;
-    IPOL(idx_dataset).COND  = idx_data_cond;
-    % mesh(X(:,:,1),Y(:,:,1),inter_XYZ(:,:,1))
-    
-end   
-    
- cond   = cat(1,IPOL.COND); 
- data   = cat(1,IPOL.DATA);
- sub    = cat(1,IPOL.SUB); 
-    
-mapped_data  = (reshape(nanmean(data(cond==1,:),1),map_dimensions(1),map_dimensions(2),map_dimensions(3)));
-mesh(X(:,:,1),Y(:,:,1),mapped_data(:,:,1))
+
+
+close all
+for k=1:3,
+    figure
+    set(gcf,'position',[100 100 [10,10].*map_dimensions(1:2)])
+    contour(X(:,:,1),Y(:,:,1),F(:,:,k),25)
+    hold on
+    colorbar
+    plot(-31,-31,'ko','markerfacecolor','k')
+end
+
+%%    Code Test by Visual Inspection / Plotting    
+x = nanmean(LAT,4);
+[h,p,ci,stats] = ttest(LAT,0,'dim',4);
+[h,p,ci,stats] = ttest(AMP,0,'dim',4);
+x = median(MEP,4);
+%x = stats.tstat;
+%x = p<0.05;
+
+hold on
+contour(X(:,:,1),Y(:,:,1),squeeze(nanmean(x,3)),25)
+view([180,-90])
+
+%colormap(diverging_bwr_40_95_c42_n256)
